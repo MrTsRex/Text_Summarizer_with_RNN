7de2540356e6fa23e325e2fe29d784635e64dc4a
Sandesh Joshi <sandeshjoshi5@gmail.com>
2018-03-11 18:35:47 -0400

    Dataset preprocessing and word vector generation code added

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Text_Summarizer_with_RNN
+﻿# Text_Summarizer_with_RNN
 
-This is a project for our bigdata ecosystem class. We're developing a Text Summarizer using Recurrent Neural Network and Sequence-to-Sequence model. The data set being used is DeepMind's CNN and Dailymail.
+This is a project for our bigdata ecosystem class. We're developing a Text Summarizer using Recurrent Neural Network and Sequence-to-Sequence model. The data set being used is DeepMind's CNN and Dailymail dataset of news stories.
---
+++ word_vector.py
@@ -0,0 +1,139 @@
+from __future__ import print_function, division
+import pandas as pd 
+import numpy as np
+import random
+from IPython.display import SVG
+import matplotlib
+
+#%matplotlib inline
+
+# nltk
+from nltk import sent_tokenize
+import nltk
+#import pydot
+nltk.download('punkt')
+
+# keras
+np.random.seed(13)
+from keras.models import Sequential
+from keras.layers import Dense, Embedding, Reshape, Activation
+from keras.utils import np_utils
+from keras.utils.data_utils import get_file
+from keras.preprocessing.text import Tokenizer
+from keras.utils.vis_utils import model_to_dot 
+from keras.preprocessing.sequence import skipgrams
+
+# We'll use A christmas carol from project gutenberg
+
+path = get_file('dickens-christmas.txt', origin="http://www.gutenberg.org/cache/epub/30368/pg30368.txt")
+corpus = open(path).read()
+
+# Split document into sentences first
+corpus = corpus[corpus.index('\n\n')+2:]  # remove header.
+sentences = sent_tokenize(corpus)
+
+# Tokenize using Keras
+base_filter='!"#$%&()*+,-./:;`<=>?@[\\]^_{|}~\t\n' + "'"
+tokenizer = Tokenizer(filters=base_filter)
+tokenizer.fit_on_texts(sentences)
+
+# Convert tokenized sentences to sequence format
+sequences = tokenizer.texts_to_sequences(sentences)
+nb_samples = sum(len(s) for s in corpus)
+
+#print(len(sequences), tokenizer.document_count)
+
+# To understand what is happening;
+
+#print(sentences[324])  # this is a sentence
+#print(sequences[324])  # this is the same sentence where words are encoded as numbers.
+#print(list(tokenizer.word_index[word.lower().replace('.', '')] 
+ #          for word in sentences[324].split()))
+
+# Let's first see how Keras' skipgrams function works.
+
+#couples, labels = skipgrams(sequences[324], len(tokenizer.word_index) + 1,
+ #   window_size=2, negative_samples=0, shuffle=True,
+  #  categorical=False, sampling_table=None)
+
+#index_2_word = {val: key for key, val in tokenizer.word_index.items()}
+
+#for w1, w2 in couples:
+ #   if w1 == 13:
+  #      print(index_2_word[w1], index_2_word[w2])
+
+ # Function to generate the inputs and outputs for all windows
+
+# Vocab size
+vocab_size = len(tokenizer.word_index) + 1
+# Dimension to reduce to
+dim = 100
+window_size = 2
+
+
+def generate_data(sequences, window_size, vocab_size):
+    for seq in sequences:
+        X, y = [], []
+        couples, _ = skipgrams(
+            seq, vocab_size,
+            window_size=window_size, negative_samples=0, shuffle=True,
+            categorical=False, sampling_table=None)
+        if not couples:
+            continue
+        for in_word, out_word in couples:
+            X.append(in_word)
+            y.append(np_utils.to_categorical(out_word, vocab_size))
+        X, y = np.array(X), np.array(y)
+        X = X.reshape(len(X), 1)
+        y = y.reshape(len(X), vocab_size)
+        yield X, y
+        
+data_generator = generate_data(sequences, window_size, vocab_size)
+
+# Create the Keras model and view it 
+#import pydot_ng as pydot
+skipgram = Sequential()
+skipgram.add(Embedding(input_dim=vocab_size, output_dim=dim, embeddings_initializer='glorot_uniform', input_length=1))
+skipgram.add(Reshape((dim,)))
+skipgram.add(Dense(input_dim=dim, units=vocab_size, activation='softmax'))
+#SVG(model_to_dot(skipgram, show_shapes=True).create(prog='dot', format='svg'))
+
+# Compile the Keras Model
+from keras.optimizers import SGD
+sgd = SGD(lr=1e-4, decay=1e-6, momentum=0.9)
+
+skipgram.compile(loss='categorical_crossentropy', optimizer="adadelta")
+
+# Fit the Skipgrams
+for iteration in range(10):
+    loss = 0
+    for x, y in generate_data(sequences, window_size, vocab_size):
+        loss += skipgram.train_on_batch(x, y)
+    print('iteration {}, loss is {}'.format(iteration, loss))
+
+word_vectors = skipgram.get_weights()[0]
+
+
+from scipy.spatial.distance import cosine
+
+
+def get_dist(w1, w2):
+    i1, i2 = tokenizer.word_index[w1], tokenizer.word_index[w2]
+    v1, v2 = word_vectors[i1], word_vectors[i2]
+    return cosine(v1, v2)
+
+def get_similarity(w1, w2):
+    return 1-get_dist(w1, w2)
+
+def get_most_similar(w1, n=10):
+    sims = {word: get_similarity(w1, word) 
+            for word in tokenizer.word_index.keys()
+            if word != w1}
+    sims = pd.Series(sims)
+    sims.sort_values(inplace=True, ascending=False)
+    return sims.iloc[:n]
+
+
+print(get_similarity('christmas', 'happy'))
+print('')
+print(get_most_similar('christmas'))
